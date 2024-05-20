150fff4ff20c11edcacb66a29a00d750ac5512f5
yakkujit <yakkujit@gmail.com>
2024-05-21 03:15:03 +0400

    fixed manifest url

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <TonConnectUIProvider manifestUrl='https://ae9d-87-241-158-77.ngrok-free.app/manifest.json'>
+  <TonConnectUIProvider manifestUrl='https://hrachocode.github.io/tondapp/manifest.json'>
     <App />
   </TonConnectUIProvider>,
 )
